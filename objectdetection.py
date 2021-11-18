43effbacb4e1a372a94ae827a11bb9564522e251
pysource7 <63154935+pysource7@users.noreply.github.com>
2021-11-18 12:20:06 +0200

    Create objectdetection.py

---
+++ objectdetection.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import urllib.request
+import os
+
+class ObjectDetection:
+    def __init__(self):
+        dnn_folder = "/content/dnn"
+        
+        print("Downloading DNN model")
+        if not os.path.exists(dnn_folder):
+          os.makedirs(dnn_folder)
+        urllib.request.urlretrieve("https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights", "/content/dnn/yolov4.weights")
+        urllib.request.urlretrieve("https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg", "/content/dnn/yolov4.cfg")
+        
+        net = cv2.dnn.readNet("/content/dnn/yolov4.weights", "/content/dnn/yolov4.cfg")
+        # Enable GPU CUDA
+        # net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+        # net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+        self.model = cv2.dnn_DetectionModel(net)
+        self.model.setInputParams(size=(608, 608), scale=1/255)
+
+        self.classes = []
+        with open("dnn_model/classes.txt", "r") as file_object:
+            for class_name in file_object.readlines():
+                class_name = class_name.strip()
+                self.classes.append(class_name)
+
+        self.colors = np.random.uniform(0, 255, size=(len(self.classes), 3))
+        
+    def detect(self, img):
+        class_ids, scores, boxes = self.model.detect(img, nmsThreshold=0.4)
+        return class_ids, scores, boxes
